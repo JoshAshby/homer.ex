c4d36da222a95b8048e692917280d41a56a07df3
Josh Ashby <joshuaashby@joshashby.com>
2025-11-16 17:35:15 -0700

    Log OTP reports to help debug crashes in prod

--- config/prod.exs
+++ config/prod.exs
@@ -14,7 +14,9 @@ config :swoosh, api_client: Swoosh.ApiClient.Req
 config :swoosh, local: false
 
 # Do not print debug messages in production
-config :logger, level: :info
+config :logger, level: :info,
+   handle_otp_reports: true,
+   handle_sasl_reports: true
 
 # Runtime production configuration, including reading
 # of environment variables, is done on config/runtime.exs.
